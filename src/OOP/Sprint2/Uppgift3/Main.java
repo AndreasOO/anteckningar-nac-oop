d9b56e46643c03b8d4aebed9813b88bc37adcf51
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-10-03 20:39:07 +0200

    Added classes and tests for assignment 3

---
+++ src/OOP/Sprint2/Uppgift3/Main.java
@@ -0,0 +1,14 @@
+package OOP.Sprint2.Uppgift3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            System.out.println("You wrote: " + input);
+        }
+    }
+}
