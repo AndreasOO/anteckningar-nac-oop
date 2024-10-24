6c5e1806b07232a0cc471c64e5c55b3d071e965a
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-10-24 21:14:42 +0200

    Added classes for assignment 10 - unfinished

--- src/OOP/Sprint3/Uppgift10ad/MyQueue.java
+++ src/OOP/Sprint3/Uppgift10ad/MyQueue.java
@@ -1,4 +1,29 @@
 package OOP.Sprint3.Uppgift10ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyQueue {
+    List<String> queue;
+
+
+    public MyQueue() {
+        queue = new ArrayList<>();
+    }
+
+    public void put(String o) {
+        queue.add(o);
+        notify();
+    }
+
+    public String take() {
+        if (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return queue.removeFirst();
+    }
 }
--- src/OOP/Sprint3/Uppgift10ad/Producer.java
+++ src/OOP/Sprint3/Uppgift10ad/Producer.java
@@ -1,4 +1,34 @@
 package OOP.Sprint3.Uppgift10ad;
 
-public class Producer {
+import java.util.Timer;
+
+public class Producer implements Runnable {
+    private static int PRODUCER_ID_INCREMENTOR = 1;
+
+    private final int producerID;
+    private int productID;
+    private final String productString;
+    private final int time;
+    private final MyQueue queue;
+
+
+    public Producer(String productString, int time, MyQueue queue) {
+        this.producerID = PRODUCER_ID_INCREMENTOR++;
+        this.productString = productString;
+        this.time = time;
+        this.queue = queue;
+
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+           queue.put(String.format("Product: %s - Product ID: %d - Producer ID: %d", this.productString, this.productID, this.producerID));
+           try {
+               wait(time);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+        }
+    }
 }
