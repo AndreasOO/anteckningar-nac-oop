fd0f0a77f7094c8315893e0a94f849bf104410d7
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-10-26 13:56:47 +0200

    Fixed url properties file and usage in assignment 14

--- src/OOP/Sprint3/Uppgift14/Main.java
+++ src/OOP/Sprint3/Uppgift14/Main.java
@@ -1,9 +1,20 @@
 package OOP.Sprint3.Uppgift14;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
+            Properties properties = new Properties();
+            try {
+                properties.load(new FileReader("src/OOP/Sprint3/Uppgift14/url-settings.properties"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-            URLReader urlReader = new URLReader("https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words.txt");
+            String url = properties.getProperty("url","");
+            URLReader urlReader = new URLReader(url);
             urlReader.readURL();
 
     }
