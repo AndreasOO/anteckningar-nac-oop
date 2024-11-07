298cecb12051375ffa8f2f234e6a0196d91a60a7
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-11-07 10:47:47 +0100

    Stress tested server with 100 clients

--- src/OOP/Sprint4/Uppgift1/Client/Main.java
+++ src/OOP/Sprint4/Uppgift1/Client/Main.java
@@ -2,13 +2,9 @@ package OOP.Sprint4.Uppgift1.Client;
 
 public class Main {
     public static void main(String[] args) {
-        Thread thread = new Thread(new Client("Andy", 44449, 0));
-        thread.start();
 
-        Thread thread2 = new Thread(new Client("Warhol", 44449, 300));
-        thread2.start();
-
-        Thread thread3 = new Thread(new Client("Genie", 44449, 800));
-        thread3.start();
+        for (int i = 0; i < 100; i++) {
+            new Thread(new Client("" + i, 44449, 100+i)).start();
+        }
     }
 }
